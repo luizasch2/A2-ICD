d85c5bb0fbd1ac7bcfbc900eeb016dbdcf4a2768
Luiza Schneider Sampaio <98579033+luizasch2@users.noreply.github.com>
2023-06-27 01:42:38 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,17 +1,6 @@
 from bokeh.models import ColumnDataSource
-from bokeh.plotting import figure, show
-from bokeh.io import output_file
 import pandas as pd 
 
 data = pd.read_csv('titanic.csv')
 
 source = ColumnDataSource(data)
-
-
-p = figure()
-p.circle(x='Survived', y='Age', source=source)
-output_file('circle.html')
-show(p)
-
-
-
