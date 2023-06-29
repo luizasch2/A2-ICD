1bb5a8c66e060bc1394b9b9c9cc82df70d9cdd09
Guilherme Queiroz Corrêa Lima <guilhermeq700@gmail.com>
2023-06-29 12:48:34 -0300

    Update graficos_gqcl.py
    Alterei o modo que estava fazendo os gráficos, dessa vez, usando objetos ColumnDataSource

--- graficos_gqcl.py
+++ graficos_gqcl.py
@@ -1,27 +1,26 @@
 import data
-from bokeh.models import FactorRange, Label, Span
-from bokeh.plotting import figure, output_file, show
-from bokeh.palettes import Blues4
+from bokeh.models import ColumnDataSource, FactorRange, Span
+from bokeh.plotting import figure, show
+from bokeh.transform import factor_cmap
 
 def bar_chart_age_sex():
-    output_file("BarChart.html")
-
     # Definindo as categorias
-    categorias = [("Crianças", "Mulheres"), ("Crianças", "Homens"), ("Adultos", "Mulheres"), ("Adultos", "Homens")]
+    categories = [("Crianças", "Mulheres"), ("Crianças", "Homens"), ("Adultos", "Mulheres"), ("Adultos", "Homens")]
 
     # Contando o número de mortos em cada categoria
-    deaths = []
-    deaths.append(len(data.data[(data.data['Sex'] == 'female') & (data.data['Age'] < 18) & (data.data['Survived'] == 0)]))
-    deaths.append(len(data.data[(data.data['Sex'] == 'male') & (data.data['Age'] < 18) & (data.data['Survived'] == 0)]))
-    deaths.append(len(data.data[(data.data['Sex'] == 'female') & (data.data['Age'] >= 18) & (data.data['Survived'] == 0)]))
-    deaths.append(len(data.data[(data.data['Sex'] == 'male') & (data.data['Age'] >= 18) & (data.data['Survived'] == 0)]))
+    deaths = [len(data.data[(data.data['Sex'] == 'female') & (data.data['Age'] < 18) & (data.data['Survived'] == 0)]),
+              len(data.data[(data.data['Sex'] == 'male') & (data.data['Age'] < 18) & (data.data['Survived'] == 0)]),
+              len(data.data[(data.data['Sex'] == 'female') & (data.data['Age'] >= 18) & (data.data['Survived'] == 0)]),
+              len(data.data[(data.data['Sex'] == 'male') & (data.data['Age'] >= 18) & (data.data['Survived'] == 0)])]
+
+    source = ColumnDataSource(data=dict(categories=categories, deaths=deaths))
 
     # Criando a figura
-    plot = figure(x_range=FactorRange(*categorias), width=850, height=480, title="Mortos por categoria")
+    plot = figure(x_range=FactorRange(*categories), width=600, height=360, title="Mortos por categoria")
 
     # Adicionando as barras
-    colors = ["#F26BD7","#5773C9","#F26BD7","#5773C9"]  # Define as cores para cada barra
-    plot.vbar(x=categorias, top=deaths, width=0.9, color=colors)
+    plot.vbar(x='categories', top='deaths', width=0.9, source=source, 
+              fill_color=factor_cmap('categories', palette=['#F26BD7', '#5773C9', '#F26BD7', '#5773C9'], factors=categories), line_color=None)
 
     plot.xgrid.grid_line_color = None
     plot.y_range.start = 0
