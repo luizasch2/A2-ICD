cfab825cbb28698d665495bec319b3bfd29d5870
Guilherme Queiroz Corrêa Lima <guilhermeq700@gmail.com>
2023-06-29 11:47:21 -0300

    Update graficos_gqcl.py

--- graficos_gqcl.py
+++ graficos_gqcl.py
@@ -17,18 +17,18 @@ def bar_chart_age_sex():
     deaths.append(len(data.data[(data.data['Sex'] == 'male') & (data.data['Age'] >= 18) & (data.data['Survived'] == 0)]))
 
     # Criando a figura
-    p = figure(x_range=FactorRange(*categorias), width=850, height=480, title="Mortos por categoria")
+    plot = figure(x_range=FactorRange(*categorias), width=850, height=480, title="Mortos por categoria")
 
     # Adicionando as barras
     colors = ["#F26BD7","#5773C9","#F26BD7","#5773C9"]  # Define as cores para cada barra
-    p.vbar(x=categorias, top=deaths, width=0.9, color=colors)
+    plot.vbar(x=categorias, top=deaths, width=0.9, color=colors)
 
-    p.xgrid.grid_line_color = None
-    p.y_range.start = 0
+    plot.xgrid.grid_line_color = None
+    plot.y_range.start = 0
 
-    p.add_layout(Span(location=deaths[1], dimension='width', line_color='black', line_dash='dashed', line_width=1))
-    p.add_layout(Span(location=deaths[3], dimension='width', line_color='black', line_dash='dashed', line_width=1))
+    plot.add_layout(Span(location=deaths[1], dimension='width', line_color='black', line_dash='dashed', line_width=1))
+    plot.add_layout(Span(location=deaths[3], dimension='width', line_color='black', line_dash='dashed', line_width=1))
 
-    show(p)
+    return plot
 
-bar_chart_age_sex()
+show(bar_chart_age_sex())
